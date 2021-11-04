8f85fb7bdd1388bc6be375984a267cb569dfa03e
InSeong-So <goflvhxj2547@gmail.com>
2021-11-04 10:13:35 +0900

    fix: ✔ Code margin, container width

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -30,7 +30,7 @@ export class Layout extends React.Component {
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
-            maxWidth: rhythm(26),
+            maxWidth: rhythm(31),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
